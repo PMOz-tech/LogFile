ac93b0b2a135179fe71d452e0a43951e8d58d47a
PMOz-tech <ozon.oghenemaro@gmail.com>
2021-11-10 11:55:11 +0100

    added

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace LogFile
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            var logFile = "log.txt";
+            //File.Create(logFile);
+
+            
+
+            Stack<DateTime> timeDate = new Stack<DateTime>();
+
+            for (int i = 0; i<20; i++)
+            {
+                timeDate.Push(DateTime.Now);
+                Console.WriteLine(timeDate.Peek());
+                Thread.Sleep(10000);
+
+            }
+
+            
+
+            Console.WriteLine("- - - - - - - - - ");
+
+            foreach (var milsec in timeDate)
+            {
+                Console.WriteLine(milsec);
+                File.AppendAllLines(logFile, new String[] { milsec.ToString() });
+            }
+        }
+    }
+}
